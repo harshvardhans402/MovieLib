1ef9e75ce7e1c78363c9c33c730adf83a516398d
harshvardhans402 <166988539+harshvardhans402@users.noreply.github.com>
2024-06-01 18:43:57 +0530

    Update apiService.js

--- frontend/src/features/apiService.js
+++ frontend/src/features/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://www.omdbapi.com/';
+const API_BASE_URL = 'https://www.omdbapi.com/';
 const API_KEY = '17895477';
 
 const fetchData = async (title,page) => {
