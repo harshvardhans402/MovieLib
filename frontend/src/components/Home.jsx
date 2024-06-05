cae5305372f04950bab027aa803a3bfa674b74b4
Harshvardhan <hash22mca@cmrit.ac.in>
2024-06-05 19:42:15 +0530

    Final Commit

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -222,6 +222,8 @@ const SearchComponent = () => {
                             movieData.map((movie) => (
                                 <motion.div
 
+                                    initial={{ opacity: 0, y: 10 }}
+                                    animate={{ opacity: 1, y: 0 }}
                                     whileInView={{ y: [10, 50, 1], opacity: [0, 0, 1] }}
                                     transition={{ duration: 0.5 }}
                                     exit={{ opacity: 0, y: 10 }}
